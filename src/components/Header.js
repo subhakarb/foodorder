274f028e14e1b714346c7fb78d7991c1502f42d8
subhakarb <subhakar.boyapati@gmail.com>
2024-11-06 13:04:16 +0530

    Tailwind CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,15 @@ const Header = () => {
     const [btnName, setBtnName] = useState("LogIn")
     const onlineStatus = useOnlineStatus();
     return (
-        <div className="flex">
-            <div className="w-150">
-                <img src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5297921.png" />
+        <div className="flex justify-between items-center p-6">
+            <div  >
+                <img className="w-24" src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5297921.png" />
             </div>
-            <div className="flex">
-                <ul>
+            <div className="flex align-middle">
+                <ul className="flex gap-3">
                     <li>Online Status: { onlineStatus ? "" : "No" }</li>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link to="/" className="text-sm/6 font-semibold text-gray-900">Home</Link>
                     </li>
                     <li>
                         <Link to="/about">About Us</Link>
